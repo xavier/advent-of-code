bf91c72d4e0e00e6a7de624486faabc249fd8fe6
Xavier Defrang <xavier.defrang@gmail.com>
2015-12-17 09:08:49 -0500

    Day 17, part 2

--- day17/day17.ex
+++ day17/day17.ex
@@ -17,7 +17,32 @@ defmodule Day17 do
   Filling all containers entirely, how many different combinations of containers
   can exactly fit all 150 liters of eggnog?
 
+  --- Part Two ---
+
+  While playing with all the containers in the kitchen, another load of eggnog arrives!
+  The shipping and receiving department is requesting as many containers as you can spare.
+
+  Find the minimum number of containers that can exactly fit all 150 liters of eggnog.
+  How many different ways can you fill that number of containers and still hold exactly 150 litres?
+
+  In the example above, the minimum number of containers was two.
+  There were three ways to use that many containers, and so the answer there would be 3.
+
   """
+
+  def find_optimal_containers(containers, quantity) do
+    by_size =
+      containers
+      |> find_containers(quantity)
+      |> Enum.group_by(fn (combo) -> Enum.count(combo) end)
+
+    smallest =
+      by_size
+      |> Dict.keys
+      |> Enum.min
+
+    by_size |> Dict.get(smallest, [])
+  end
 
   def find_containers(containers, quantity) do
     containers
@@ -44,25 +69,39 @@ defmodule Day17Test do
 
   setup do
     {:ok, %{
-      containers: [
+      test_containers: [
+        20, 15, 10, 5, 5
+      ],
+      input_containers: [
         11, 30, 47, 31, 32, 36, 3, 1, 5, 3, 32, 36, 15, 11, 46, 26, 28, 1, 19, 3,
       ]}
     }
   end
 
-  test "simple" do
-    containers = [20, 15, 10, 5, 5]
-
+  test "acceptance part 1", %{test_containers: containers} do
     combos = Day17.find_containers(containers, 25)
 
     assert 4 == Enum.count(combos)
   end
 
-  test "input 1", %{containers: containers} do
+  test "acceptance part 2", %{test_containers: containers} do
+    combos = Day17.find_optimal_containers(containers, 25)
+
+    assert 3 == Enum.count(combos)
+  end
+
+  test "input 1", %{input_containers: containers} do
     IO.puts "input 1"
     containers
     |> Day17.find_containers(150)
+    |> Enum.count
     |> IO.inspect
+  end
+
+  test "input 2", %{input_containers: containers} do
+    IO.puts "input 2"
+    containers
+    |> Day17.find_optimal_containers(150)
     |> Enum.count
     |> IO.inspect
   end
