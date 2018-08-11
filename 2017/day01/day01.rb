69f688f214aad2c6756006127e949c00e6aba9ad
Xavier Defrang <xavier.defrang@gmail.com>
2018-08-11 16:18:54 +0200

    2017 day 1 part 2

--- 2017/day01/day01.rb
+++ 2017/day01/day01.rb
@@ -5,20 +5,36 @@ class String
   end
 end
 
-def sum(digits)
+def sum(digits, steps)
   digits
     .each_with_index
     .reduce(0) do |acc, (digit, index)|
-      next_digit = digits[index.succ % digits.length]
+      next_digit = digits[(index + steps) % digits.length]
       digit == next_digit ? acc + digit : acc
     end
 end
 
-# puts sum("1122".digits)
-# puts sum("1111".digits)
-# puts sum("1234".digits)
-# puts sum("91212129".digits)
+def sum_part1(digits)
+  sum(digits, 1)
+end
 
-INPUT = File.read("input.txt").strip
+def sum_part2(digits)
+  sum(digits, digits.length / 2)
+end
 
-puts sum(INPUT.digits)
+INPUT = File.read("input.txt").strip.digits
+
+# Part 1
+# puts sum_part1("1122".digits)
+# puts sum_part1("1111".digits)
+# puts sum_part1("1234".digits)
+# puts sum_part1("91212129".digits)
+puts sum_part1(INPUT)
+
+# Part 2
+#puts sum_part2("1212".digits)
+#puts sum_part2("1221".digits)
+#puts sum_part2("123425".digits)
+#puts sum_part2("123123".digits)
+#puts sum_part2("12131415".digits)
+puts sum_part2(INPUT)
