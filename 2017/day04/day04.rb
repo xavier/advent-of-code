78b75ebf559ccc59e4925a203d726bfc5f71a561
Xavier Defrang <xavier.defrang@gmail.com>
2018-08-12 12:39:37 +0200

    2017 day 4 part 2

--- 2017/day04/day04.rb
+++ 2017/day04/day04.rb
@@ -1,11 +1,17 @@
 require "set"
 
-def valid?(passphrase)
+def valid_part1?(passphrase)
   words = passphrase.split()
+  words.count == Set.new(words).count
+end
+
+def valid_part2?(passphrase)
+  words = passphrase.split().map { |word| word.chars.sort.join }
   words.count == Set.new(words).count
 end
 
 INPUT = File.read("input.txt").split("\n")
 
 puts INPUT.count
-puts INPUT.count { |passphrase| valid?(passphrase) }
+puts INPUT.count { |passphrase| valid_part1?(passphrase) }
+puts INPUT.count { |passphrase| valid_part2?(passphrase) }
