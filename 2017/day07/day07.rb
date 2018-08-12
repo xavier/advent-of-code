be93af77c9add3191e96f6380b1a2d1d17ddd683
Xavier Defrang <xavier.defrang@gmail.com>
2018-08-12 15:15:21 +0200

    2017 day 7 part 2

--- 2017/day07/day07.rb
+++ 2017/day07/day07.rb
@@ -16,6 +16,10 @@ def parse_line(line)
   end
 end
 
+TEST = parse(File.read("test.txt"))
+INPUT = parse(File.read("input.txt"))
+
+# Part 1
 
 def find_root(definitions)
   candidates = Set.new
@@ -29,8 +33,59 @@ def find_root(definitions)
   (candidates - all_children).to_a.fetch(0)
 end
 
-TEST = parse(File.read("test.txt"))
-INPUT = parse(File.read("input.txt"))
-
 puts find_root(TEST)
 puts find_root(INPUT)
+
+# Part 2
+
+Program = Struct.new(:name, :weight, :children)
+
+class Tower
+
+  attr_reader :programs
+
+  def initialize(defintions)
+    @programs = defintions.each_with_object({}) do |(name, weight, children), hash|
+      hash[name] = Program.new(name, weight, children)
+    end
+  end
+
+  def weigh(name)
+    program = @programs.fetch(name)
+    program.children.reduce(program.weight) { |total, child| total + weigh(child) }
+  end
+
+  def find_imbalance()
+    @programs.keys.select { |name| !check_balance(name).nil? }
+  end
+
+  def check_balance(name)
+    program = @programs.fetch(name)
+    return if program.children.none?
+
+    weights = program.children.each_with_object({}) { |child, hash| hash[child] = weigh(child) }
+    return if weights.values.uniq.count == 1
+
+    weights
+  end
+
+end
+
+tower_test = Tower.new(TEST)
+
+# p tower_test.check_balance("ugml")
+# p tower_test.check_balance("padx")
+# p tower_test.check_balance("fwft")
+# p tower_test.check_balance("tknk")
+#p tower_test.find_imbalance()
+
+tower_input = Tower.new(INPUT)
+
+# Narrow down
+tower_input.find_imbalance().each do |name|
+  puts name
+  p tower_input.check_balance(name)
+end
+
+# Inspect weight to adjust
+p tower_input.programs.fetch("egbzge")
