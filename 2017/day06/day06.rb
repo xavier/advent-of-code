8b017cab8aca28d43ca0c8b05d16e404a45b5524
Xavier Defrang <xavier.defrang@gmail.com>
2018-08-12 13:55:51 +0200

    2017 day 6 part 1

---
+++ 2017/day06/day06.rb
@@ -0,0 +1,45 @@
+require "set"
+
+class Memory
+
+  def initialize(banks)
+    @banks = banks
+  end
+
+  def count_cycles_until_same
+    states = Set.new()
+    until states.include?(@banks)
+      states.add(@banks)
+      cycle!
+    end
+    states.count
+  end
+
+  def cycle!
+    blocks, start_index = @banks.each_with_index.max_by { |blocks, idx| blocks }
+    redistribute(blocks, start_index)
+  end
+
+  def redistribute(blocks, idx)
+    @banks[idx] = 0
+    blocks.times do
+      idx = idx.succ % @banks.count
+      @banks[idx] += 1
+    end
+  end
+
+  def to_s
+    @banks.inspect
+  end
+
+end
+
+INPUT = File.read("input.txt").split().map(&:to_i)
+
+# Test case
+
+test = Memory.new([0, 2, 7, 0])
+puts test.count_cycles_until_same
+
+part1 = Memory.new(INPUT.dup)
+puts part1.count_cycles_until_same
