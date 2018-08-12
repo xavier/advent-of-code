f60b4cfc36bbfede632fc130778b8d7b945c8583
Xavier Defrang <xavier.defrang@gmail.com>
2018-08-12 13:07:20 +0200

    2017 day 5 part 2

--- 2017/day05/day05.rb
+++ 2017/day05/day05.rb
@@ -5,7 +5,7 @@ class CPU
   attr_reader :steps
 
   def initialize(jump_offsets)
-    @jump_offsets = jump_offsets
+    @jump_offsets = jump_offsets.dup
     @pc = 0
     @steps = 0
   end
@@ -14,12 +14,17 @@ class CPU
     loop do
       puts(to_s) if debug
       offset = @jump_offsets.fetch(@pc)
-      @jump_offsets[@pc] += 1
+      @jump_offsets[@pc] += act_strange(offset)
+      act_strange(offset)
       @pc += offset
       @steps += 1
     end
   rescue IndexError
     nil
+  end
+
+  def act_strange(offset)
+    1
   end
 
   def to_s
@@ -31,12 +36,33 @@ class CPU
 
 end
 
+class CPU2 < CPU
+
+  def act_strange(offset)
+    offset < 3 ? 1 : -1
+  end
+
+end
+
+
 INPUT = File.read("input.txt").split("\n").map(&:to_i)
+
+# Part 1
 
 cpu = CPU.new([0, 3, 0, 1, -3])
 cpu.run(debug: true)
 puts cpu.steps
 
 cpu = CPU.new(INPUT)
+cpu.run
+puts cpu.steps
+
+# Part 2
+
+cpu = CPU2.new([0, 3, 0, 1, -3])
+cpu.run()
+puts cpu.steps
+
+cpu = CPU2.new(INPUT)
 cpu.run
 puts cpu.steps
