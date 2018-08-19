0c59b15e3eaf57e4da69e52137a51ed53baa23e3
Xavier Defrang <xavier.defrang@gmail.com>
2018-08-19 12:18:10 +0200

    2017 day 22 part 2

--- 2017/day22/day22.rb
+++ 2017/day22/day22.rb
@@ -18,20 +18,28 @@ class Grid
   attr_reader :size
 
   def initialize(size)
-    @size  = size
-    @nodes = Hash.new { |h, k| h[k] = Set.new }
+    @size = size
+    @nodes = Hash.new { |h, k| h[k] = Hash.new(:clean) }
   end
 
-  def infected?(x, y)
-    @nodes[y].include?(x)
+  def weaken(x, y)
+    @nodes[y][x] = :weakened
   end
 
   def infect(x, y)
-    @nodes[y].add(x)
+    @nodes[y][x] = :infected
+  end
+
+  def flag(x, y)
+    @nodes[y][x] = :flagged
   end
 
   def clean(x, y)
-    @nodes[y].delete(x)
+    @nodes[y][x] = :clean
+  end
+
+  def state(x, y)
+    @nodes[y][x]
   end
 
 end
@@ -58,14 +66,16 @@ class Virus
   end
 
   def burst
-    if @grid.infected?(@x, @y)
+    case @grid.state(@x, @y)
+    when :infected
       @grid.clean(@x, @y)
       turn(RIGHT)
-    else
+    when :clean
       @grid.infect(@x, @y)
       turn(LEFT)
-
       @infections += 1
+    else
+      raise ArgumentError
     end
     move_forward
     self
@@ -78,12 +88,16 @@ class Virus
     (-size..size).map do |y|
       (-size..size).map do |x|
         format = (@x == x && @y == y) ? FORMAT_CURRENT : FORMAT_OTHER
-        sprintf(format, @grid.infected?(x, y) ? "#" : ".")
+        sprintf(format, dump_cell(x, y))
       end.join("")
     end.join("\n")
   end
 
-  private
+  protected
+
+  def dump_cell(x, y)
+    @grid.state(x, y) == :infected ? "#" : "."
+  end
 
   def turn(towards_direction)
     @direction = (@direction + towards_direction) % 4
@@ -97,6 +111,50 @@ class Virus
 
 end
 
+class Virus2 < Virus
+
+  AROUND = 2
+
+  def burst
+    case @grid.state(@x, @y)
+    when :clean
+      @grid.weaken(@x, @y)
+      turn(LEFT)
+
+    when :weakened
+      @grid.infect(@x, @y)
+      @infections += 1
+
+    when :infected
+      @grid.flag(@x, @y)
+      turn(RIGHT)
+
+    when :flagged
+      @grid.clean(@x, @y)
+      turn(AROUND)
+
+    else
+      raise ArgumentError
+
+    end
+    move_forward
+  end
+
+  protected
+
+  CELLS = {
+    clean: ".",
+    weakened: "W",
+    infected: "#",
+    flagged: "F",
+  }
+
+  def dump_cell(x, y)
+    CELLS.fetch(@grid.state(x, y))
+  end
+
+end
+
 
 TEST = File.read("test.txt")
 INPUT = File.read("input.txt")
@@ -104,15 +162,28 @@ INPUT = File.read("input.txt")
 # Test
 test_virus = Virus.new(Grid.parse(TEST))
 10_000.times do |n|
-  #puts "-- #{n} --" * 10
-  #puts test_virus.dump
   test_virus.burst
 end
 puts "=====" * 10
 puts test_virus.dump
 p test_virus.infections
 
+test_virus2 = Virus2.new(Grid.parse(TEST))
+100.times do |n|
+  #puts "-- #{n} --" * 10
+  #puts test_virus2.dump
+  test_virus2.burst
+end
+puts "=====" * 10
+puts test_virus2.dump
+p test_virus2.infections
+
 # Part 1
 puzzle_virus = Virus.new(Grid.parse(INPUT))
 10_000.times { puzzle_virus.burst }
 p puzzle_virus.infections
+
+# Part 2
+puzzle_virus2 = Virus2.new(Grid.parse(INPUT))
+10_000_000.times { puzzle_virus2.burst }
+p puzzle_virus2.infections
